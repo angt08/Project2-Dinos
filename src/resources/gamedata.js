9a7e43361443d3044403adb3273862534b4e230f
ang <angiezafra07@gmail.com>
2019-10-22 23:39:11 -0400

    adding more game data

--- src/resources/gamedata.js
+++ src/resources/gamedata.js
@@ -1,3 +1,5 @@
+
+
 const gameData = {
 
   prompt: `You wake in a study room, peer out a window.
@@ -9,37 +11,37 @@ const gameData = {
   },
 
   "Explore the house":{
-    prompt:"You've ventured into the house ",
+        prompt:"You've ventured into the house ",
     "You found the kitchen...chow time": {
 
-     prompt: "A banquet, sweet!",
+        prompt: "A banquet, sweet!",
       "Let's try this roast mutton": {
         prompt: "Does this  mutton taste funny to you?..",
         "":null
       },
 
-      "drink this wine,from this fine looking chalice": {
-              prompt: " Hey man, I don't think that was wine..",
-        
+      "Drink this wine,from this fine looking chalice": {
+        prompt: " Hey man, I don't think that was wine..",
+        "":null
       },
-
     },
 
     "Hey there's a hidden doorway here explore... ": {
-      prompt: " A beast lies here, you've woken it, it begins to charge at you...",
+        prompt: " A beast lies here, you've woken it, it begins to charge at you...",
       
     "You're no wimp,fight!...": {
-              prompt:" Oh, What made you think you could take on an unknown beast with no weapons or armor?",
+      prompt: `Oh, 
+        What made you think you could take on an unknown beast with no weapons or armor?`,
       },
       "Nah, run! run so fast!": {
-              prompt: `You've run out the house,
+        prompt: `You've run out the house,
                  but you're in the middle of nowhere...`,
         "Run accross a road and into the woods": {
-              prompt: "Running into the woods, classic ",
-          "Drop to the ground ball up and cry": {
-            prompt: "You hear whispering nearby",
+        prompt: "Running into the woods, classic ",
+          "Drop to the ground ball up, begin to ugly cry silently...": {
+                prompt: "Wait,sshh, you hear whispering nearby?",
             "You see a figure in the distance frantically waving": {
-              promp: "You're not sure who that is, but given the circumstances",
+                promp: "You're not sure , but given the circumstances..",
               "You army crawl towards this unknown figure": null,
               "":null,
             },
@@ -47,7 +49,7 @@ const gameData = {
           "Run back to the house":null,
         },
         "Walk around the house, maybe there's a shed": { 
-          prompt:"No shed, there's a path to the gated garden, try that..",
+                prompt:"No shed, there's a path to the gated garden, try that..",
         },
 
       },
